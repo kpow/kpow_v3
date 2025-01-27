eb69a4c0aa954ba11fba944ab9bf8631eec8d9b1
kpowz <39087935-kpowz@users.noreply.replit.com>
2025-01-27 00:50:21 +0000

    Agent query: Can you access the /phish-stats page now? It should show a loading state since we need to set up the API key.
    Remove Layout component from Router.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/e5d0a08d-1c65-4d75-836d-4c7c550ea75f/98fae95f-28b4-4977-b606-3feded9aefca.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { Layout } from "@/components/Layout";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
 import PhishStats from "@/pages/PhishStats";
@@ -10,14 +9,12 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/phish-stats" component={PhishStats} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/phish-stats" component={PhishStats} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
