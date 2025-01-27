80087b7590a05ee4526a81fbbb6e2d1bf3cf3037
kpowz <39087935-kpowz@users.noreply.replit.com>
2025-01-27 00:52:48 +0000

    Checkpoint before revert - Build the initial prototype
    Wrap router in Layout component
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/e5d0a08d-1c65-4d75-836d-4c7c550ea75f/39af27c1-13b7-4b9a-8b6c-67042ffe2329.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
+import { Layout } from "@/components/Layout";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
 import PhishStats from "@/pages/PhishStats";
@@ -9,12 +10,14 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/phish-stats" component={PhishStats} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/phish-stats" component={PhishStats} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   );
 }
 
