439cbe420872e608077998887ef31468bdfd5354
kpowz <39087935-kpowz@users.noreply.replit.com>
2025-01-28 05:16:20 +0000

    Restored to 'ad6c465dc6a29f6c967f55f5e788122e469f5085'
    Replit-Restored-To:ad6c465dc6a29f6c967f55f5e788122e469f5085

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { Layout } from "@/components/Layout";
-import Home from "@/pages/home";
+import Home from "@/pages/Home";
 import About from "@/pages/About";
 import ShowStats from "@/pages/show-stats";
 import NotFound from "@/pages/not-found";
