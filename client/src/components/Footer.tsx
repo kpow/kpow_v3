97a2680ba7acf25f95e0cfacba8cc376fe2c94ae
Kevin Power <kpow@kpow.com>
2025-01-30 04:34:37 +0000

    updated footer content

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,20 +6,13 @@ export function Footer() {
       <div className="container max-w-6xl mx-auto px-4 flex gap-8 justify-between">
         {/* About Section */}
         <div className="flex items-start gap-4 max-w-sm">
-          <div className="flex-shrink-0">
-            <img
-              src="/kpow_avatar.jpg"
-              alt="Kpow"
-              className="w-12 h-12 rounded-full"
-            />
-          </div>
+          <div className="flex-shrink-0"></div>
           <div>
             <h3 className="text-xl font-bold mb-1">About kpow</h3>
             <p className="text-sm text-gray-300 leading-relaxed">
-              Digital Architect - Leader - Developer - Pixel Farmer.
-              <br />
-              Voracious Reader and Dad. I'm into travel, ukes, pugs, live music,
-              and pixels.
+              Digital Architect - Leader - Developer - Pixel Farmer. I'm a
+              voracious reader and boogie-down dad. I'm into travel, ukes, pugs,
+              live music, and pixels.
             </p>
           </div>
         </div>
@@ -28,7 +21,9 @@ export function Footer() {
         <div className="max-w-md">
           <h3 className="text-xl font-bold mb-1">Site</h3>
           <p className="text-sm text-gray-300 leading-relaxed">
-            This is a JAMstack site built with Next.js, React.js, Material-UI, React-Query, and some content in Markdown. Using Instagram, GoodReads, Feedbin, Unsplash, faviconkit and whatever other API's I'm playing with :)
+            This is a Node.js site built with React.js, Shadcn-UI, React-Query,
+            and some content in Markdown. Using Instagram, GoodReads, Feedbin,
+            Phish.net, Github and whatever other API's I'm playing with :)
           </p>
         </div>
       </div>
