b5ae6c43f1c49e0aabaf5cecde1e6431d101ff63
kpowz <39087935-kpowz@users.noreply.replit.com>
2025-02-02 03:37:00 +0000

    Restored to '5789b0b56d3d8863fcc0bcfb33f3077b143448f6'
    Replit-Restored-To:5789b0b56d3d8863fcc0bcfb33f3077b143448f6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,6 @@ import Books from "@/pages/Books";
 import StarredArticles from "@/pages/StarredArticles";
 import PMonk from "@/pages/PMonk";
 import Videos from "@/pages/Videos";
-import Battle from "@/pages/Battle";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -27,7 +26,6 @@ function Router() {
         <Route path="/videos/page/:page" component={Videos} />
         <Route path="/videos" component={Videos} />
         <Route path="/pmonk" component={PMonk} />
-        <Route path="/battle" component={Battle} />
         <Route component={NotFound} />
       </Switch>
     </Layout>
