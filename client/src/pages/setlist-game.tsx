880aa209fbd766caab88b3edac435664726f0498
kpowz <39087935-kpowz@users.noreply.replit.com>
2025-02-13 20:00:38 +0000

    Restored to '8772b13c24a6777400c2f692e65493ed75c53258'
    Replit-Restored-To:8772b13c24a6777400c2f692e65493ed75c53258

--- client/src/pages/setlist-game.tsx
+++ client/src/pages/setlist-game.tsx
@@ -3,9 +3,9 @@ import { SetlistGame } from "@/components/setlist-game";
 
 export default function SetlistGamePage() {
   return (
-    <div className="container mx-auto px-4 lg:px-8 min-h-screen flex flex-col">
-      <PageTitle size="lg" className="mb-4 md:mb-6 lg:mb-8">phish setlist game</PageTitle>
-      <div className="flex-grow flex">
+    <div className="container mx-auto p-4 min-h-screen flex flex-col">
+      <PageTitle size="lg" className="mb-4">phish setlist game</PageTitle>
+      <div className="flex-grow">
         <SetlistGame />
       </div>
     </div>
