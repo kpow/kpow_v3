ba2661dbe8a7b60ab48cf47ecfd20be1ab8a8304
kpowz <39087935-kpowz@users.noreply.replit.com>
2025-02-20 17:22:05 +0000

    Restored to 'c1b457010d00cb68e015929fa1d60172788353f7'
    Replit-Restored-To:c1b457010d00cb68e015929fa1d60172788353f7

--- client/src/pages/CubePage.tsx
+++
@@ -1,9 +0,0 @@
-import { CubeGame } from "@/components/CubeGame/CubeGame";
-
-export default function CubePage() {
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <CubeGame />
-    </div>
-  );
-}
